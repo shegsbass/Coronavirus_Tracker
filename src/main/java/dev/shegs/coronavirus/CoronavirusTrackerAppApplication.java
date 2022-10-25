4427caec7beb95dd84217cfed52fb668499a0694
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2022-10-25 13:22:20 +0100

    Cron execute the fetchVirusData method everyday

--- src/main/java/dev/shegs/coronavirus/CoronavirusTrackerAppApplication.java
+++ src/main/java/dev/shegs/coronavirus/CoronavirusTrackerAppApplication.java
@@ -2,8 +2,10 @@ package dev.shegs.coronavirus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronavirusTrackerAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/dev/shegs/coronavirus/services/CoronaVirusDataService.java
+++ src/main/java/dev/shegs/coronavirus/services/CoronaVirusDataService.java
@@ -2,6 +2,7 @@ package dev.shegs.coronavirus.services;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -18,6 +19,7 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL= "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct //PostConstruct will make the application begin from here
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
