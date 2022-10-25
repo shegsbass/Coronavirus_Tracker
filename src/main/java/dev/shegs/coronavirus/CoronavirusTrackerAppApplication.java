254f73e751e2702e023be35432b63e647576f8c6
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2022-10-25 13:12:40 +0100

    Looping through csv file to get records by header

---
+++ src/main/java/dev/shegs/coronavirus/CoronavirusTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package dev.shegs.coronavirus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/shegs/coronavirus/CoronavirusTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shegs.coronavirus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
